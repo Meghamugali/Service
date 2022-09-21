02f006fbf1426abb80db0b2622ec9ce75210d100
Megha <meghabmugali@gmail.com>
2022-09-21 12:11:45 +0530

    Initial commit--playing music file in service

---
+++ app/src/main/java/com/example/service/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void serviceHandler(View view) {
+        Intent serviceIntent = new Intent(this,CateringService.class);
+        switch (view.getId()){
+            case R.id.btnStart:
+                serviceIntent.putExtra("mkey","musicfilename.mp3");
+                startService(serviceIntent);
+                break;
+            case R.id.btnStop:
+                stopService(serviceIntent);
+                break;
+        }
+    }
+}
